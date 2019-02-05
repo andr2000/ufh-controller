c5bb41976a43c668e7e7b0f206d7c37e90b0a271
Oleksandr Andrushchenko <andr2000@gmail.com>
2019-02-05 15:52:46 +0200

    Add sample SQL query from boiler and weather tables
    Signed-off-by: Oleksandr Andrushchenko <andr2000@gmail.com>

---
+++ queries/flow_and_outside.sql
@@ -0,0 +1,19 @@
+SELECT DateTime,  Toutside, Flow FROM
+
+(
+SELECT
+	DATETIME(datetime_unix, 'unixepoch', 'localtime') AS DateTime ,
+	(t_sinoptik_10/10.) AS Toutside,
+	NULL AS Flow
+FROM Weather
+
+UNION
+
+SELECT
+	DATETIME(datetime_unix, 'unixepoch', 'localtime') AS DateTime ,
+	NULL AS Toutside,
+	(temp_flow_100/100.) AS Flow
+FROM Boiler
+)
+
+ORDER BY DateTime;
