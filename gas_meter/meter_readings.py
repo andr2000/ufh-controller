14a09c551e7ea57fd65545ba74e915ed5be79a94
Oleksandr Andrushchenko <andr2000@gmail.com>
2020-01-19 19:52:34 +0200

    Implement multiple regression analysis for gas meter readings
    Signed-off-by: Oleksandr Andrushchenko <andr2000@gmail.com>

--- gas_meter/meter_readings.py
+++ gas_meter/meter_readings.py
@@ -1,52 +1,58 @@
 #!/usr/bin/env python3
-import itertools
-import numpy
+import numpy as np
+from sklearn import linear_model
 
-log_file = open('meter_readings.log', 'r')
+FIELD_DATETIME = 0
+FIELD_METER = 1
+FIELD_HC = 2
+FIELD_HWC = 3
+FIELD_VALID = 4
+FIELD_COMMENT = 5
+
+log_file = open('meter_readings.csv', 'r')
 log = log_file.readlines()
 
+METER = []
+HC = []
+HWC = []
 print('Stored readings:\n')
-data = []
 for line in log:
-    data.append(line.strip().split(';'))
-    print('\t%s' % line)
+    s = line.strip().split(',')
+    if int(s[FIELD_VALID]):
+        METER.append(float(s[FIELD_METER]))
+        HC.append(int(s[FIELD_HC]))
+        HWC.append(int(s[FIELD_HWC]))
+        print('\t%s' % line)
+    else:
+        print('\tSkipping ' + line)
 
-# FIXME: get rid of repeated elements: AB BA
-perms = itertools.permutations(data, 2)
-for i in perms:
-    # Form matrices: the result of the permutations above is:
-    # (['2020-01-11 22:55:00', '1849.662', '1735840618', '96401092']
-	#  ['2020-01-11 21:12:00', '1848.412', '1733683455', '96401092'])
-    eq1 = i[0]
-    eq2 = i[1]
-    print('Solving\n\t%s\n\t%s\n' %(eq1, eq2))
-    A = numpy.array([[int(eq1[2]), int(eq1[3])], [int(eq2[2]), int(eq2[3])]])
-    B = numpy.array([float(eq1[1]), float(eq2[1])])
-    try:
-        X = numpy.linalg.inv(A).dot(B)
-    except numpy.linalg.LinAlgError:
-        # Singular matrix: this can happen if meter readings
-        # have adjusted, but BAI counters not: for instance if other consumers
-        # than BAI were active
-        print('\tSkipping\n')
-        pass
-    print(X)
+clf = linear_model.LinearRegression()
+y = np.array(METER)
+X = np.array([np.array(HC), np.array(HWC)])
+# transpose so input vectors are along the rows
+X = X.T
+# add bias term
+X = np.c_[X, np.ones(X.shape[0])]
+beta_hat = np.linalg.lstsq(X, y, rcond=None)[0]
+
+print('\nMultiple regression:')
+print(beta_hat)
 
 #https://techniczny.wordpress.com/2018/04/08/pomiar-zuzycia-gazu-przez-raspberry-pi-i-ebus/
-print('\nCoefficient for the sum of Hc1 and Hwc1\n')
-last = len(data) - 1
-last_reading = float(data[last][1])
-last_hc1 = int(data[last][2])
-last_hwc1 = int(data[last][3])
-last_cal = (last_hc1 + last_hwc1) / last_reading
+print('\nCoefficient for the sum of Hc1 and Hwc1')
+last = len(METER) - 1
+last_reading = METER[last]
+last_hc1 = HC[last]
+last_hwc1 = HWC[last]
+last_sum = last_hc1 + last_hwc1
+last_cal = last_sum / last_reading
 print('Meter: %s calibration %s\n' % (last_reading, last_cal))
 
-for i in data:
-    meter = float(i[1])
-    hc1 = int(i[2])
-    hwc1 = int(i[3])
-    cur_sum = hc1 + hwc1
-    last_sum = last_hc1 + last_hwc1
-    readings = (last_reading + (cur_sum - last_sum) / last_cal)
-    print('Reading: %.3f real %.3f error %.3f' %
-          (readings, meter, abs(meter - readings)))
+for i in range(last + 1):
+    cur_sum = HC[i] + HWC[i]
+    readings_sum = (last_reading + (cur_sum - last_sum) / last_cal)
+    readings_mregr = beta_hat[0] * HC[i] + beta_hat[1] * HWC[i] + beta_hat[2]
+    print('Reading: meter %.3f sum cal %.3f error %.3f; multi regression: %.3f '
+          'error %.3f ' %
+          (METER[i], readings_sum, abs(METER[i] - readings_sum),
+           readings_mregr, abs(METER[i] - readings_mregr)))
