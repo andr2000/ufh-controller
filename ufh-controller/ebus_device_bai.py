ae5821b1ce4a793a59dab236a726209fdf7962a3
Oleksandr Andrushchenko <andr2000@gmail.com>
2020-01-26 13:32:19 +0200

    ufh: Report gas meter readings
    Signed-off-by: Oleksandr Andrushchenko <andr2000@gmail.com>

--- ufh-controller/ebus_device_bai.py
+++ ufh-controller/ebus_device_bai.py
@@ -23,7 +23,7 @@ class EbusDeviceBAI(EbusDevice):
         super().__init__(scan_result)
 
         self.tbl = TableLogger(
-            columns='timestamp,FlowDes,Flow,Return,Flame,Power,PowKW,WaterPres,PumpPow,Status01,Status02,SetModeR',
+            columns='timestamp,FlowDes,Flow,Return,Flame,Power,PowKW,WaterPres,PumpPow,Status01,Status02,SetModeR,m3',
             formatters={
                 'timestamp': '{:%Y-%m-%d %H:%M:%S}'.format,
                 'FlowDes': '{:,.2f}'.format,
@@ -34,11 +34,12 @@ class EbusDeviceBAI(EbusDevice):
                 'PowKW': '{:,.2f}'.format,
                 'PumpPow': '{:,.2f}'.format,
                 'WaterPres': '{:,.3f}'.format,
+                'm3': '{:,.3f}'.format,
             },
             colwidth={
                 'Flow': 6, 'FlowDes': 6, 'Return': 6, 'Flame': 5, 'Power': 5,
                 'PowKW': 5, 'WaterPres': 9, 'PumpPow': 7, 'Status01': 25,
-                'Status02': 20, 'SetModeR': 27
+                'Status02': 20, 'SetModeR': 27, 'm3': 9
             })
 
         self.power_max_hc_kw = float(self.read_0('PartloadHcKW'))
@@ -119,10 +120,16 @@ class EbusDeviceBAI(EbusDevice):
             res = self.read_0(param)
             pr_energy_count_hwc1 = res
 
+            # Calculate gas meter readings
+            hc = int(pr_energy_sum_hc1)
+            hwc = int(pr_energy_sum_hwc1)
+            X = [1.1480950277684922e-06, 6.31354528893052e-07, -204.44259713590995]
+            m3 = X[0] * hc + X[1] * hwc + X[2]
+
             self.tbl(datetime.datetime.now(), float(temp_flow_des),
                      float(temp_flow), float(temp_return), flame, float(power),
                      power_kw, float(water_pressure), float(pump_power),
-                     status01, status02, set_mode_r)
+                     status01, status02, set_mode_r, m3)
 
             # store to the database
             values = {
@@ -144,8 +151,9 @@ class EbusDeviceBAI(EbusDevice):
                 'PrEnergyCountHwc1': pr_energy_count_hwc1}
             database.store_burner(values)
 
-            telegram.send_message('Flow %s Return %s Power, kW %.3f Flame %s PumpPower %s' %
-                                  (temp_flow, temp_return, power_kw, flame, pump_power))
+            telegram.send_message('Flow %s Return %s Power, kW %.3f Flame %s PumpPower %s m3=%.3f Hc %s Hwc %s' %
+                                  (temp_flow, temp_return, power_kw, flame, pump_power, m3,
+                                   pr_energy_sum_hc1, pr_energy_sum_hwc1))
         except ValueError as e:
             telegram.send_message('BAI: Error: %s = %s' % (param, res))
             self.logger.error('%s: %s = %s' % (str(e), param, res))
